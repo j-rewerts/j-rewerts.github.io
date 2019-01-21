f4856a7ada9fa6d186a84b4d2cb056aca814300b
Jared Rewerts <jaredrewerts@gmail.com>
2019-01-21 15:32:09 -0700

    Added styling and code for active page detection.

--- js/onload.js
+++ js/onload.js
@@ -1,3 +1,20 @@
-window.onload = () => {
-  console.log('Loaded')
-}
+(() => {
+  window.onload = () => {
+    console.log('Loaded')
+    getNavByUrl(window.location.href)
+      .className += ' active';
+  }
+
+  const getNavByUrl = (navUrl) => {
+    const elements = document.getElementsByClassName('nav-item');
+    const nav = Array.prototype.filter.call(elements, element => {
+      return element.href === navUrl;
+    });
+
+    if (nav.length !== 1) {
+      throw new Error('Unable to exactly match nav item.');
+    }
+
+    return nav[0];
+  }
+})();
